46e8e19f778f25adb44d3169120d7dda380a1b20
oxi <oxi@keks.li>
2020-11-29 13:49:22 +0100

    kissfft compiled as js module

--- wwwroot/kissfft.js
+++ wwwroot/kissfft.js
@@ -1,14 +1,30 @@
+import Module from "./a.out.js"
+
+let instance = {
+    ready: new Promise(resolve => {
+        Module({
+            onRuntimeInitialized() {
+                //instance = Promise.resolve();
+                instance = Object.assign(this, {
+                    ready: Promise.resolve()
+                });
+                resolve();
+            }
+        });
+    })
+};
+
 /** Compute the FFT of a real-valued mxn matrix. */
-function rfft2d(data, m, n) {
+export function rfft2d(data, m, n) {
     /* Allocate input and output arrays on the heap. */
     var heapData = allocFromArray(data);
     var heapSpectrum = alloc(2*m*n*4);
 
-    _rfft2d(heapData.byteOffset, heapSpectrum.byteOffset, m, n);
+    instance._rfft2d(heapData.byteOffset, heapSpectrum.byteOffset, m, n);
 
     /* Get spectrum from the heap, copy it to local array. */
     var spectrum = new Float32Array(m*n*2);
-    spectrum.set(new Float32Array(Module.HEAPU8.buffer,
+    spectrum.set(new Float32Array(instance.HEAPU8.buffer,
                                   heapSpectrum.byteOffset, m*n*2));
 
     /* Free heap objects. */
@@ -20,14 +36,14 @@ function rfft2d(data, m, n) {
 
 
 /** Compute the inverse FFT of a real-valued mxn matrix. */
-function irfft2d(spectrum, m, n) {
+export function irfft2d(spectrum, m, n) {
     var heapSpectrum = allocFromArray(spectrum);
     var heapData = alloc(m*n*4);
 
-    _irfft2d(heapSpectrum.byteOffset, heapData.byteOffset, m, n);
+    instance._irfft2d(heapSpectrum.byteOffset, heapData.byteOffset, m, n);
 
     var data = new Float32Array(m*n);
-    data.set(new Float32Array(Module.HEAPU8.buffer,
+    data.set(new Float32Array(instance.HEAPU8.buffer,
                               heapData.byteOffset, m*n));
 
     for (i=0;i<m*n;i++) {
@@ -42,7 +58,7 @@ function irfft2d(spectrum, m, n) {
 
 
 /** Create a heap array from the array ar. */
-function allocFromArray(ar) {
+export function allocFromArray(ar) {
     /* Allocate */
     var nbytes = ar.length * ar.BYTES_PER_ELEMENT;
     var heapArray = alloc(nbytes);
@@ -54,13 +70,13 @@ function allocFromArray(ar) {
 
 
 /** Allocate a heap array to be passed to a compiled function. */
-function alloc(nbytes) {
-    var ptr = Module._malloc(nbytes);
-    return new Uint8Array(Module.HEAPU8.buffer, ptr, nbytes);
+export function alloc(nbytes) {
+    var ptr = instance._malloc(nbytes);
+    return new Uint8Array(instance.HEAPU8.buffer, ptr, nbytes);
 }
 
 
 /** Free a heap array. */
-function free(heapArray) {
-    Module._free(heapArray.byteOffset);
+export function free(heapArray) {
+    instance._free(heapArray.byteOffset);
 }
