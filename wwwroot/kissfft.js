cec06160e91e413c006996229600a784b1b652b0
ChristianToennes <christian.toennes@keks.li>
2022-06-22 23:38:21 +0200

    updated fft library to include complex 2d/3d and real 3d

--- wwwroot/kissfft.js
+++ wwwroot/kissfft.js
@@ -1,5 +1,3 @@
-self.importScripts("./a.out.js")
-
 /** Compute the FFT of a real-valued mxn matrix. */
 function rfft2d(data, m, n) {
     /* Allocate input and output arrays on the heap. */
@@ -32,7 +30,7 @@ function irfft2d(spectrum, m, n) {
     data.set(new Float32Array(Module.HEAPU8.buffer,
                               heapData.byteOffset, m*n));
 
-    for (var i=0;i<m*n;i++) {
+    for (i=0;i<m*n;i++) {
         data[i] /= m*n;
     }
 
@@ -41,7 +39,130 @@ function irfft2d(spectrum, m, n) {
 
     return data;
 }
+/** Compute the FFT of a real-valued mxn matrix. */
+function fft2d(data, m, n) {
+    /* Allocate input and output arrays on the heap. */
+    var heapData = allocFromArray(data);
+    var heapSpectrum = alloc(2*m*n*4);
 
+    _fft2d(heapData.byteOffset, heapSpectrum.byteOffset, m, n);
+
+    /* Get spectrum from the heap, copy it to local array. */
+    var spectrum = new Float32Array(m*n*2);
+    spectrum.set(new Float32Array(Module.HEAPU8.buffer,
+                                  heapSpectrum.byteOffset, m*n*2));
+
+    /* Free heap objects. */
+    free(heapData);
+    free(heapSpectrum);
+
+    return spectrum;
+}
+
+
+/** Compute the inverse FFT of a real-valued mxn matrix. */
+function ifft2d(spectrum, m, n) {
+    var heapSpectrum = allocFromArray(spectrum);
+    var heapData = alloc(m*n*4);
+
+    _ifft2d(heapSpectrum.byteOffset, heapData.byteOffset, m, n);
+
+    var data = new Float32Array(m*n*2);
+    data.set(new Float32Array(Module.HEAPU8.buffer,
+                              heapData.byteOffset, m*n*2));
+
+    for (i=0;i<m*n*2;i++) {
+        data[i] /= m*n;
+    }
+
+    free(heapSpectrum);
+    free(heapData);
+
+    return data;
+}
+/** Compute the FFT of a real-valued mxn matrix. */
+function rfft3d(data, m, n, l) {
+    /* Allocate input and output arrays on the heap. */
+    var heapData = allocFromArray(data);
+    var heapSpectrum = alloc(2*m*n*l*4);
+
+    _rfft3d(heapData.byteOffset, heapSpectrum.byteOffset, m, n, l);
+
+    /* Get spectrum from the heap, copy it to local array. */
+    var spectrum = new Float32Array(m*n*l*2);
+    spectrum.set(new Float32Array(Module.HEAPU8.buffer,
+                                  heapSpectrum.byteOffset, m*n*l*2));
+
+    /* Free heap objects. */
+    free(heapData);
+    free(heapSpectrum);
+
+    return spectrum;
+}
+
+
+/** Compute the inverse FFT of a real-valued mxn matrix. */
+function irfft3d(spectrum, m, n, l) {
+    var heapSpectrum = allocFromArray(spectrum);
+    var heapData = alloc(m*n*l*4);
+
+    _irfft3d(heapSpectrum.byteOffset, heapData.byteOffset, m, n, l);
+
+    var data = new Float32Array(m*n*l);
+    data.set(new Float32Array(Module.HEAPU8.buffer,
+                              heapData.byteOffset, m*n*l));
+
+    for (i=0;i<m*n*l;i++) {
+        data[i] /= m*n*l;
+    }
+
+    free(heapSpectrum);
+    free(heapData);
+
+    return data;
+}
+
+/** Compute the FFT of a real-valued mxn matrix. */
+function fft3d(data, m, n, l) {
+    /* Allocate input and output arrays on the heap. */
+    var heapData = allocFromArray(data);
+    var heapSpectrum = alloc(2*m*n*l*4);
+
+    _fft3d(heapData.byteOffset, heapSpectrum.byteOffset, m, n, l);
+
+    /* Get spectrum from the heap, copy it to local array. */
+    var spectrum = new Float32Array(m*n*l*2);
+    spectrum.set(new Float32Array(Module.HEAPU8.buffer,
+                                  heapSpectrum.byteOffset, m*n*l*2));
+
+    /* Free heap objects. */
+    free(heapData);
+    free(heapSpectrum);
+
+    return spectrum;
+}
+
+
+/** Compute the inverse FFT of a real-valued mxn matrix. */
+function ifft3d(spectrum, m, n, l) {
+    var heapSpectrum = allocFromArray(spectrum);
+    var heapData = alloc(m*n*l*4);
+
+    _ifft3d(heapSpectrum.byteOffset, heapData.byteOffset, m, n, l);
+
+    var data = new Float32Array(m*n*l*2);
+    data.set(new Float32Array(Module.HEAPU8.buffer,
+                              heapData.byteOffset, m*n*l*2));
+
+    for (i=0;i<m*n*l*2;i++) {
+        data[i] /= m*n*l;
+    }
+
+    free(heapSpectrum);
+    free(heapData);
+
+    return data;
+}
 
 /** Create a heap array from the array ar. */
 function allocFromArray(ar) {
